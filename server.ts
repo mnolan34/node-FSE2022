c1d8b6320f45ecef3903fc62a38bca2765a4d571
nolanmatt34 <nolan.matt@northeastern.edu>
2022-02-07 09:53:11 -0500

    Updated Server

--- server.ts
+++ server.ts
@@ -1,11 +1,28 @@
 import express, {Request, Response} from 'express';
+//Import Controllers
+import UserController from "./controllers/UserController";
+import TuitController from "./controllers/TuitController";
+
+//Mongoose control
+import mongoose from "mongoose";
+mongoose.connect("mogodb://localhost:27017/tuiter");
+
+//App Controll
 const app = express();
+app.use(express.json());
+
+//Adding based on Piazza
 
 app.get('/hello', (req: Request, res: Response) =>
     res.send('Hello World!'));
 
 app.get('/add/:a/:b', (req: Request, res: Response) =>
     res.send(req.params.a + req.params.b));
+
+//Controller Instances
+//const userController = UserController.getInstance(app);
+//const tuitController = TuitController.getInstance(app);
+
 
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
