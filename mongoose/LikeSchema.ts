1cd793aa4c25f63cf39c66f97cea6f59e92b4d14
nolanmatt34 <nolan.matt@northeastern.edu>
2022-02-21 10:16:42 -0500

    Removed Comma in LikeSchema

--- mongoose/LikeSchema.ts
+++ mongoose/LikeSchema.ts
@@ -3,7 +3,7 @@ import Like from "../models/Like";
 
 const LikeSchema = new mongoose.Schema<Like>({
     tuit: {type: Schema.Types.ObjectId, ref: "TuitModel"},
-    likedBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
+    likedBy: {type: Schema.Types.ObjectId, ref: "UserModel"}
 }, {collection: "likes"});
 
 export default LikeSchema;
