448e963e8a11d6ababf96af69a1c37a4acad010e
nolanmatt34 <nolan.matt@northeastern.edu>
2022-02-23 14:31:18 -0500

    Added Follow Controller

---
+++ controllers/FollowController.ts
@@ -0,0 +1,47 @@
+import {Express, Request, Response} from "express";
+import FollowDao from "../daos/FollowDao";
+import FollowControllerI from "../interfaces/FollowControllerI";
+
+export default class FollowController implements FollowControllerI {
+    private static followDao: FollowDao = FollowDao.getInstance();
+    private static followController: FollowController | null = null;
+
+    public static getInstance = (app: Express): FollowController => {
+        if (FollowController.followController === null) {
+            FollowController.followController = new FollowController();
+            app.get("/api/users/:uidFollowing/follows/:uidFollower",
+                FollowController.followController.findAllUsersFollowingUser);
+            app.get("/api/users/:uidFollower/follows/:uidFollowing",
+                FollowController.followController.findAllUsersFollowedByUser);
+            app.delete("/api/users/:uidFollowing/follows/:uidFollower",
+                FollowController.followController.userUnFollowsUser);
+            app.post("/api/users/:uidFollowing/follows/:uidFollower",
+                FollowController.followController.userFollowsUser);
+        }
+
+        return FollowController.followController;
+    }
+
+    private constructor() {}
+
+    findAllUsersFollowingUser = (req: Request, res: Response) =>
+        FollowController.followDao.findAllUsersFollowingUser
+        (req.params.uid)
+            .then(follows => res.json(follows));
+
+    findAllUsersFollowedByUser = (req: Request, res: Response) =>
+        FollowController.followDao.findAllUsersFollowedByUser
+        (req.params.uid)
+            .then(follows => res.json(follows));
+
+    userUnFollowsUser = (req: Request, res: Response) =>
+        FollowController.followDao.userUnFollowsUser
+        (req.params.uidFollowing, req.params.uidFollower)
+            .then(status => res.json(status));
+
+    userFollowsUser = (req: Request, res: Response) =>
+        FollowController.followDao.userFollowsUser
+        (req.params.uidFollowing, req.params.uidFollower)
+            .then(follows => res.json(follows));
+
+}
