736def22e5123e31ff47d84b184aac26de5a2244
nolanmatt34 <nolan.matt@northeastern.edu>
2022-02-23 10:39:26 -0500

    Updated server.ts with mongo url and name

--- server.ts
+++ server.ts
@@ -6,13 +6,13 @@ import LikeController from "./controllers/LikeController";
 
 //Mongoose control
 import mongoose from "mongoose";
-
+var cors = require('cors');
 
 const PROTOCOL = "mongodb+srv";
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASS;
-const DB_URL = process.env.DB_URL;
-const DB_NAME = "Tuiter";
+const DB_URL = "cluster0.v4of9.mongodb.net";
+const DB_NAME = "myFirstDatabase";
 const DB_QUERY = "retryWrites=true&w=majority";
 
 const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${DB_URL}/${DB_NAME}?${DB_QUERY}`;
@@ -21,10 +21,14 @@ mongoose.connect(connectionString);
 //App Control
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //Adding based on Piazza
 app.get('/hello', (req: Request, res: Response) =>
     res.send('Hello World!'));
+
+app.get('/', (req: Request, res: Response) =>
+    res.send('Welcome!'));
 
 app.get('/add/:a/:b', (req: Request, res: Response) =>
     res.send(req.params.a + req.params.b));
