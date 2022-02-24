6a8b856328eabcb48ba3b61f3e1ac48606a4d454
nolanmatt34 <nolan.matt@northeastern.edu>
2022-02-24 13:56:46 -0500

    Implemented BM interface and Dao

---
+++ daos/BookmarkDao.ts
@@ -0,0 +1,34 @@
+import BookmarkDaoI from "../interfaces/BookmarkDaoI";
+import BookmarkModel from "../mongoose/BookmarkModel";
+import Bookmark from "../models/Bookmark";
+
+export default class BookmarkDao implements BookmarkDaoI{
+    //Implement singleton pattern
+    private static bookmarkDao: BookmarkDao | null = null;
+    public static getInstance = (): BookmarkDao => {
+        if(BookmarkDao.bookmarkDao === null){
+            BookmarkDao.bookmarkDao = new BookmarkDao();
+        }
+        return BookmarkDao.bookmarkDao;
+    }
+
+    private constructor() {}
+
+    userBookmarksTuit =
+        async (tid: string, uid: string): Promise<Bookmark> =>
+            BookmarkModel
+                .create({bookmarkedTuit: tid, bookmarkedBy: uid});
+
+    userUnBookmarksTuit =
+        async (tid: string, uid: string): Promise<any> =>
+            BookmarkModel
+                .deleteOne({bookmarkedTuit: tid, bookmarkedBy: uid});
+
+    userViewsBookmarkList =
+        async (uid: string): Promise<Bookmark[]> =>
+            BookmarkModel
+                .find({bookmarkedBy: uid})
+                .populate("bookmarkedTuit")
+                .exec();
+    /* Custom Two Down below */
+};
---
+++ interfaces/BookmarkDaoI.ts
@@ -0,0 +1,8 @@
+import Bookmark from "../models/Bookmark";
+
+export default interface BookmarkDaoI{
+    userBookmarksTuit (tid: string, uid: string): Promise<Bookmark>;
+    userUnBookmarksTuit (tid: string, uid: string): Promise<any>;
+    userViewsBookmarkList (uid: string): Promise<Bookmark[]>;
+    /* Custom Two Down below */
+};
