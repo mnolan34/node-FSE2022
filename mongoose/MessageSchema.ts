730c2f0aa7929f3225fb6727ccf64554b294decc
nolanmatt34 <nolan.matt@northeastern.edu>
2022-02-25 11:28:02 -0500

    Changed Message Schema for required message, and sentON

--- mongoose/MessageSchema.ts
+++ mongoose/MessageSchema.ts
@@ -2,10 +2,10 @@ import mongoose, {Schema} from "mongoose";
 import Message from "../models/Message";
 
 const MessageSchema = new mongoose.Schema<Message>({
-    message: String,
+    message: {type: String, required: true},
     to: {type: Schema.Types.ObjectId, ref: "UserModel"},
     from: {type: Schema.Types.ObjectId, ref: "UserModel"},
-    sentOn: Date
+    sentOn: {type: Date, default: Date.now}
 }, {collection: "messages"});
 
 export default MessageSchema;
