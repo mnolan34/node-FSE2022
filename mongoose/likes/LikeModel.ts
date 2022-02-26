df2a7578a501c8a3425aaf12562190f1e988c37a
nolanmatt34 <nolan.matt@northeastern.edu>
2022-02-25 19:09:06 -0500

    Final Push of A2, final JS Docs implemented and ran

--- mongoose/likes/LikeModel.ts
+++ mongoose/likes/LikeModel.ts
@@ -1,4 +1,7 @@
-/** From Lecture Notes */
+/**
+ * @file Implements mongoose model to CRUD
+ * documents in the likes collection
+ */
 import mongoose from "mongoose";
 import LikeSchema from "./LikeSchema";
 const LikeModel = mongoose.model("LikeModel", LikeSchema);
